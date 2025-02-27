cf87279766f321dc7f42c4a2068eea60158a8ab7
Yu Wang <wangyu16@users.noreply.github.com>
2025-02-27 14:11:23 -0600

    Create testFunction.js

---
+++ test/testFunction.js
@@ -0,0 +1,14 @@
+// myFunction.js
+export function myFunction(param) {
+  return `Processed: ${param}`;
+}
+
+// You can export multiple functions
+export function anotherFunction() {
+  return "Hello from another function";
+}
+
+// You can also have a default export
+export default function defaultFunction() {
+  return "I am the default function";
+}
